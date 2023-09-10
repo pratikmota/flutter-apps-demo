5e7fde413022de585151d70342f7198f2b9f84d8
Pratik Mota <mota.pratik@gmail.com>
2023-09-10 22:55:07 +0530

    dice initial code

---
+++ roll_dice_app/lib/gradiant_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomCenter;
+
+class GradiantContainer extends StatelessWidget {
+  const GradiantContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StylesText("Hello"),
+      ),
+    );
+  }
+}
---
+++ roll_dice_app/lib/styled_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StylesText extends StatelessWidget {
+  const StylesText(this.text, {super.key});
+  final String text;
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 50,
+        color: Color.fromARGB(255, 217, 216, 209),
+      ),
+    );
+  }
+}
